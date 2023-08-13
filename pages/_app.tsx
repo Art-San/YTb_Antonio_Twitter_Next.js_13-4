539d8de314339d763b25a721c2b99fa514c0dbc1
Aleksander <asava@sibmail.com>
2023-08-13 14:06:32 +0700

    Auth UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import Layout from '@/components/Layout'
-import Modal from '@/components/Modal'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal isOpen />
+      {/* <Modal actionLabel="Submit" isOpen={true} title="Test Modal" /> */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
