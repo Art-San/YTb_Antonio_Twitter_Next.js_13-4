3786a10af45b5e30b4da26bfef27d1205967645b
Aleksander <asava@sibmail.com>
2023-08-20 22:49:13 +0700

    Like & Follow Functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import PostFeed from '@/components/posts/PostFeed'
 import Header from '@/components/Header'
 import Form from '@/components/Form'
 
@@ -5,7 +6,8 @@ export default function Home() {
   return (
     <>
       <Header label="Home" />
-      {/* <Form placeholder="What's happening?" /> */}
+      <Form placeholder="What's happening?" />
+      <PostFeed />
     </>
   )
 }
